6ac98e8a928bc9a29e97c2f75b957090a035d170
Alessandro Oggioni <oggioniale@gmail.com>
2021-02-01 12:30:43 +0100

    Add NEWS.md

--- R/_start.R
+++ R/_start.R
@@ -1,4 +1,15 @@
 # start with R package
+# follow https://www.r-bloggers.com/2017/08/building-a-website-with-pkgdown-a-short-guide/
+require(devtools)
+use_readme_rmd()
+use_news_md()
+use_vignette("test")  #substitute with the name of your package
+
+
+
+
+
+
 # 1. https://support.rstudio.com/hc/en-us/articles/200486488-Developing-Packages-with-RStudio
 
 # documentation with roxygen2
