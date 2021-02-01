6330320c4d382a7a0572f18a3a78b2748dd2e663
Alessandro Oggioni <oggioniale@gmail.com>
2021-02-01 16:10:50 +0100

    website for ReLTER package

--- R/_start.R
+++ R/_start.R
@@ -3,24 +3,20 @@
 require(devtools)
 use_readme_rmd()
 use_news_md()
-use_vignette("test")  #substitute with the name of your package
-
-
-
-
-
-
-# 1. https://support.rstudio.com/hc/en-us/articles/200486488-Developing-Packages-with-RStudio
-
-# documentation with roxygen2
-roxygen2::roxygenise()
-
-# Prerequisites pkgdown
-require(devtools)
-use_readme_rmd()
-use_news_md()
 use_vignette("ReLTER")  #substitute with the name of your package
+
 use_github_links()
-# creating a bare-bones website with pkgdown
-# devtools::install_github("hadley/pkgdown")
-pkgdown::build_site()
+
+# use_travis()
+use_cran_badge() 
+
+# man subfolder:
+devtools::document()
+
+# my_package.Rmd:
+devtools::build_vignettes()
+
+# bare_bones website
+devtools::install_githb("hadley/pkgdown")
+library(pkgdown)
+build_site()
