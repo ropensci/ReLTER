06489429de038a963ba45ae466c2cc0d55d2cce7
Alessandro Oggioni <oggioniale@gmail.com>
2021-04-22 18:19:55 +0200

    package startup message

---
+++ R/onAttach.R
@@ -0,0 +1,9 @@
+.onAttach <- function(libname, pkgname){
+  # to show a startup message
+  startMessage <- c(
+    'ReLTER is specially drafted for the LTER community.\n\n',
+    'Contribute to the improvement of it, join the group of developers (https://github.com/oggioniale/ReLTER).\n\n',
+    'If you use this package, please cite it (citation(\'ReLTER\')).\n\n'
+  )
+  packageStartupMessage(startMessage) 
+}
